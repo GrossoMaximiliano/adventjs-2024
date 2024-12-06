91a338e700189bbf6dd1326d964755d76aa5542c
GrossoMaximiliano <87657806+GrossoMaximiliano@users.noreply.github.com>
2024-12-05 22:57:01 -0300

    Create index.js

---
+++ 5/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {{ type: 'I' | 'R', size: number }[]} shoes
+ * @returns {number[]} Available shoes 
+ */
+function organizeShoes(shoes) {
+  let contador = {};
+  let result = [];
+
+  for( let shoe of shoes ){
+    const {type, size} = shoe;
+    const sizeS = `${size}`; 
+    const talle = contador[sizeS] ?? { "I": 0, "R": 0};
+    talle[type] += 1;
+
+    const par = type === "R" ? "I" : "R";
+
+    if ( talle[type] > 0 && talle[par] > 0 ) {
+      result.push(size);
+      talle[type]--;
+      talle[par]--;
+    }
+
+    contador[sizeS] = talle;
+  }
+
+  return result
+}
