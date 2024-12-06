96e6a3ec5107afa6d48e11b89b782873423125e3
GrossoMaximiliano <87657806+GrossoMaximiliano@users.noreply.github.com>
2024-12-05 22:32:16 -0300

    Create index.js

---
+++ 4/index.js
@@ -0,0 +1,17 @@
+function createXmasTree(height, ornament) {
+  let arbol = [];
+  let vacioXlado = height - 1;
+
+  for(let i = 0; i < height; i++){
+    const lado = ornament.repeat(i);
+    const vacio = "_".repeat(vacioXlado - lado.length) 
+
+    arbol.push(`${vacio}${lado}${ornament}${lado}${vacio}`);
+  }
+  
+  const troncoVacio = "_".repeat(vacioXlado);
+  arbol.push( troncoVacio + "#" +  troncoVacio );
+  arbol.push( troncoVacio + "#" +  troncoVacio );
+
+  return arbol.join("\n")
+}
